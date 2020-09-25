79bbfbc176d8de24089d43469d3a6bae8a731396
Yew Heng Chong <yewheng@YewHengss-Macbook.local>
2020-09-25 11:44:49 +0800

    add /login jwt token

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, make_response
-import requests
+from requests import Request, Session
 import json
 import os
 import jwt
@@ -14,24 +14,31 @@ def home():
 
 @app.route('/validation', methods=['GET'])
 def login():
-    data = {
-        "user" : "caritativofiona",
-    "password" : "922cb712bea79bc8"
-    }
-
     user_credentials = {
-        "user" : 'caritativofiona',
+        'user' : 'caritativofiona',
         'password' : '922cb712bea79bc8'
     }
-
     #user_credentials = request.form['input_name'].json()
+
+    request = Request(
+        method = 'POST',
+        url = server_url + "login",
+        data = user_credentials
+    )
+
+    prepped = request.prepare()
+    session = Session()
+    response = session.send(prepped)
+
+    if response.status_code == 200:
+        jwt_token = response.text
+    else:
+        jwt_token = None
 
     if user_credentials['user'] != data['user'] or user_credentials['password'] != data['password']:
         return make_response('Invalid Credentials', 401, {'WWW-Authenticate' : 'Basic-realm=Login Required!'})
     # Token generation from POSTMAN
-    r = requests.get("http://techtrek2020.ap-southeast-1.elasticbeanstalk.com/login")
-    
-
+    # r = requests.get("http://techtrek2020.ap-southeast-1.elasticbeanstalk.com/login")
     #return "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImNhcml0YXRpdm9maW9uYSIsImlhdCI6MTYwMTAwMjIzMywiZXhwIjoxNjAxMDAyODMzLCJpc3MiOiJ0ZWNodHJlazIwMjAifQ.hboi7QHJzzXGUvpb653msp106zOOnL01_Tn_LUrMn78BA1KeAoGuBhgp4Pa11cgJnrsWCok6x0pMxal4AVR9oQ"
 
 @app.route('/logout')
