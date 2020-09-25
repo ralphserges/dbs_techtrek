89fc8190eb72ff9bbf01d3bba66204d8f2f1cf23
liuzian <liuzian94@gmail.com>
2020-09-25 14:07:21 +0800

    linked login back and front

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
-<<<<<<< HEAD
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
 import requests
-=======
 from flask import Flask, render_template, request, make_response
 from requests import Request, Session
->>>>>>> 53c12e3fdeb5c70313571e4238dc9d0b6fe4b559
+from flask_sqlalchemy import SQLAlchemy
 import json
 import os
 
 app= Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app) 
+
 
 class Customer_DB(db.Model):
     id = db.Column(db.Integer, primary_key=True) 
@@ -25,32 +23,27 @@ class Customer_DB(db.Model):
 
 server_url = "http://techtrek2020.ap-southeast-1.elasticbeanstalk.com/"
 
+def get_token():
+    
+    payload = {
+        "username" : "caritativofiona",
+        "password" : "922cb712bea79bc8"
+    }
+    url = server_url+"login" 
+    r = requests.post(url, data=payload)
+    return r.text
+
+
 @app.route('/')
-@app.route('/home')
-def home_page():
-
-    url = "http://techtrek2020.ap-southeast-1.elasticbeanstalk.com/validateForm"
-
-    payload = {"customerName":"james",
-             "customerAge":"29", 
-             "serviceOfficerName":"officer123", 
-             "NRIC": "S5454545B", 
-             "registrationTime":"25/12/2020 08:12:55", 
-             "branchCode":"120", 
-             "image":"",
-             "productType":["137","070"]}
-
-    token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImNhcml0YXRpdm9maW9uYSIsImlhdCI6MTYwMTAxMDI1MywiZXhwIjoxNjAxMDEwODUzLCJpc3MiOiJ0ZWNodHJlazIwMjAifQ.Ozt4Fi0-9FeE_swLW1Mx-K8WA3f_WyhJxHHnv1nK4mzdLyDn3M5cI7QyjfRQ68rgZq4H0d8lK_YUWsO4JNkVtQ"
-    headers = {"Authorization": "Bearer " + token}
-    r = requests.post(url, data=payload, headers=headers)
-
-    return str(r.text)
-
-
+@app.route('/login')
+def login_page():
+    return render_template("login.html")
+    
+   
 @app.route('/form', methods=['POST','GET'])
 def form_page():
-    
-    return render_template('form.html')
+    username = request.form['username']
+    return render_template('form.html', username=username)
 
 
 @app.route('/userconfirm', methods=['POST'])
@@ -69,8 +62,6 @@ def user_confirm():
     #db.session.commit()
    
     return render_template('userconfirm.html', user=cust_name)
-
-
 
 @app.route('/validation', methods=['GET'])
 def login():
@@ -93,6 +84,7 @@ def login():
     )
 
     if response.status_code == 200:
+        token = response.text
         return response.text
     else:
         return "Bad Request"
@@ -106,7 +98,28 @@ def login():
 @app.route('/logout')
 def logout():
     return ''
->>>>>>> 53c12e3fdeb5c70313571e4238dc9d0b6fe4b559
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+'''
+ url = "http://techtrek2020.ap-southeast-1.elasticbeanstalk.com/validateForm"
+
+    payload = {
+            "customerName":"james",
+             "customerAge":"29", 
+             "serviceOfficerName":"officerjohn", 
+             "NRIC": "S5454545B", 
+             "registrationTime":"25/12/2020 08:12:55", 
+             "branchCode":"7171", 
+             "image":"",
+             "productType":["Investor","Insurance"]
+    }
+
+    token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImNhcml0YXRpdm9maW9uYSIsImlhdCI6MTYwMTAxMjQ3OCwiZXhwIjoxNjAxMDEzMDc4LCJpc3MiOiJ0ZWNodHJlazIwMjAifQ.hHn78450WsIOTn59u6lPPZCsmhMQfCrFeT6Qlf656Pxo8mu8Gry5-O6b-uaiqXUM3BPaDznRuXSG2Qgwoe1uDg"
+    headers = {"Authorization": "Bearer " + token}
+    r = requests.post(url, data=payload, headers=headers)
+    
+    return str(get_token())
+
+'''
