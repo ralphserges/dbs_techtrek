444d6808fe725ddc3892c22d81bc97effc6bc633
liuzian <liuzian94@gmail.com>
2020-09-25 10:50:11 +0800

    added form.html as layout

--- app.py
+++ app.py
@@ -9,8 +9,12 @@ app= Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
-def home():
+def home_page():
     return render_template('home.html')
+
+@app.route('/form')
+def form_page():
+    return render_template('form.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
